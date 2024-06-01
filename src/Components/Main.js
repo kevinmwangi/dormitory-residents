5f6dfc6b027650137bf865da9d113c6556ac8585
Kelvin.Mwangi <Kelvin.Mwangi@squaddigital.com>
2024-06-01 18:31:43 +0300

    Pushing search

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -10,11 +10,21 @@ function Main(STUDENTS) {
         const student = STUDENTS.find(student => student.name.toLowerCase() === name.toLowerCase());
         if (!student) {
             setError(`Sorry, ${name} is not a student here` );
-        } else if (!validStudent(date, student.validity)) {
+        } else if (!validateStudent(date, student.validity)) {
             setError(`Sorry, ${name}'s validity is expired`);
         }else {
             setResidents([...residents, name]);
         }
+    };
+
+    const validateStudent = (joiningDate, validityDate) => {
+        const dateNow = new Date();
+        const today = new Date(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate());
+        const [year, month, day] = joiningDate.split('_');
+        const [yyy, mm, dd] = validityDate.split('_');
+        const maxValid = new Date(yyy, mm - 1, dd);
+        const selected = new Date(year, month - 1, day);
+        return (maxValid >= selected) && (maxValid  >= today);
     };
 
   return (
