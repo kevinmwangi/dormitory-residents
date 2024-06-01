a586243cd6b6a7d975a953137223dd5b11b74f9e
Kelvin.Mwangi <Kelvin.Mwangi@squaddigital.com>
2024-06-01 18:46:11 +0300

    Pushing search

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Error({messages}) {
 	if (!messages) {
 		return null;
-	};
+	}
 	return (
 		<div data-testid="errorMsg" className="alert error mt-20 slide-up-fade-in">{
 			{messages.map((message, index) => (
